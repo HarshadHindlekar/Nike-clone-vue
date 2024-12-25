0d8d232aac1b81a9c9156c58d3d0b12ca49efe34
Harshad Hindlekar <harshad@cloudfinch.com>
2024-12-25 12:17:52 +0530

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-
-import { MotionPlugin } from '@vueuse/motion'
-import VueSmoothScroll from "v-smooth-scroll";
-createApp(App).use(MotionPlugin).use(VueSmoothScroll, {
-    duration: 2000,
-}).mount("#app");
